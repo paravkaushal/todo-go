62bee2986eac5e6db0b74cbfe16c90a9646e4bd4
Parav Kaushal <paravkaushal.kv1@gmail.com>
2022-05-27 18:22:32 +0530

    add handler & create server

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/thedevsaddam/renderer"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -42,7 +45,36 @@ func init() {
 	db = sess.DB(dbName)
 }
 
- 
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Mount("/todo", todoHandlers())
+
+	srv := &http.Server{
+		Addr:         port,
+		Handler:      r,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+	go func() {
+		log.Println("Listening on port", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen:%s\n", err)
+		}
+	}()
+}
+func todoHandlers() http.Handler {
+	rg := chi.NewRouter()
+	rg.Group(func(r chi.Router) {
+		r.Get("/", fetchTodos)
+		r.Post("/", createTodo)
+		r.Put("/{id}", updateTodo)
+		r.Delete("/{id}", deleteTodo)
+	})
+	return rg
+}
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
